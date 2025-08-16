87bfe9a981401e82c4c50e8223a71c8173fadadb
TanTrieuNguyen <132246693+TanTrieuNguyen@users.noreply.github.com>
2025-08-16 17:25:39 +0700

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
+import os
 from flask import Flask, request, jsonify
-from flask_cors import CORS   # thêm dòng này
-import os, importlib.util
+import importlib.util
 
 PREDICT_SCRIPT_PATH = os.path.join("backend", "predict_disease_dl.py")
 spec = importlib.util.spec_from_file_location("predict_module", PREDICT_SCRIPT_PATH)
@@ -9,7 +9,6 @@ spec.loader.exec_module(predict_module)
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-CORS(app)  # Cho phép tất cả domain gọi API (fix lỗi Failed to fetch)
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -22,8 +21,6 @@ def predict():
             return jsonify({"error": "Thiếu dữ liệu 'symptoms' dạng list"}), 400
 
         result = predict_module.predict_disease(symptoms)
-
-        # Rút gọn xác suất
         probs = result.pop("all_probabilities", {})
         if isinstance(probs, dict):
             sorted_probs = sorted(probs.items(), key=lambda x: x[1], reverse=True)
@@ -36,4 +33,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    port = int(os.environ.get("PORT", 5000))  # Render sẽ cấp PORT ngẫu nhiên
+    app.run(host="0.0.0.0", port=port)
